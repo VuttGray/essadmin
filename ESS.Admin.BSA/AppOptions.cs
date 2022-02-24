7939a30cebc726e0e77d23c30ae4e5615960e9af
Stepanov <vutt.gray@gmail.com>
2022-02-24 23:15:17 +0300

    Add project dependencies and the main message grid

---
+++ ESS.Admin.BSA/AppOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESS.Admin.BSA
+{
+    public class AppOptions
+    {
+        public bool ShowDeleted { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
--- ESS.Admin.BSA/Program.cs
+++ ESS.Admin.BSA/Program.cs
@@ -1,11 +1,34 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using ESS.Admin.BSA;
+using ESS.Admin.Core.Abstractions.Repositories;
+using ESS.Admin.Core.Abstractions.Services;
+using ESS.Admin.Core.Application.Services;
+using ESS.Admin.DataAccess;
+using ESS.Admin.DataAccess.Data;
+using ESS.Admin.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var appOptions = builder.Configuration.Get<AppOptions>();
+builder.Services.AddSingleton(appOptions);
+builder.Services.Configure<AppOptions>(builder.Configuration);
+builder.Services.AddScoped<IMessageService, MessageService>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDevExpressBlazor();
+
+// Repository
+builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+builder.Services.AddScoped<IDbInitializer, EfDbInitializer>();
+
+// Database
+builder.Services.AddDbContextFactory<DataContext>(x =>
+{
+    x.UseSqlServer(appOptions.ConnectionString);
+    x.UseUpperSnakeCaseNamingConvention();
+    x.UseLazyLoadingProxies();
+});
 
 var app = builder.Build();
 
